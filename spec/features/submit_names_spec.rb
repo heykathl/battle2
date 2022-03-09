6feebd522a1787f78bce8834453b0e5670f8709e
Adam Woodcock <adam.woodcock98@gmail.com>
2022-03-09 15:03:05 +0000

    Wrote feature test to look for hit points, implemented HTML

--- spec/features/submit_names_spec.rb
+++ spec/features/submit_names_spec.rb
@@ -1,5 +1,9 @@
 feature 'submitting names' do
 
+  before do
+
+  end
+
   scenario 'view configuration' do
     visit '/'
     expect(page).to have_content('Player 1')
@@ -14,6 +18,21 @@ feature 'submitting names' do
     fill_in("player_1", with: "Lats")
     fill_in("player_2", with: "Cats")
     click_button 'Play'
-    expect(page).to have_content('Lats vs Cats')
+    expect(page).to have_content('Lats v Cats')
   end
+end
+
+feature 'hit points' do
+
+  before do
+    visit '/'
+    fill_in("player_1", with: "Lats")
+    fill_in("player_2", with: "Cats")
+    click_button 'Play'
+  end
+
+  scenario 'view player 2s hit points' do
+    expect(page).to have_css('.hp', text: 'HP 100')
+  end 
+
 end
