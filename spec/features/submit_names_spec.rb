4882e75f063be14d4ef32ed5fe6e712bc17e7ab8
Kathleen Ly <lykathleen@outlook.com>
2022-03-09 15:12:13 +0000

    Added test helpers to DRY code

--- spec/features/submit_names_spec.rb
+++ spec/features/submit_names_spec.rb
@@ -1,9 +1,5 @@
 feature 'submitting names' do
 
-  before do
-
-  end
-
   scenario 'view configuration' do
     visit '/'
     expect(page).to have_content('Player 1')
@@ -14,24 +10,15 @@ feature 'submitting names' do
   end
 
   scenario 'page should submit names' do
-    visit '/'
-    fill_in("player_1", with: "Lats")
-    fill_in("player_2", with: "Cats")
-    click_button 'Play'
+    sign_in_and_play
     expect(page).to have_content('Lats v Cats')
   end
 end
 
 feature 'hit points' do
 
-  before do
-    visit '/'
-    fill_in("player_1", with: "Lats")
-    fill_in("player_2", with: "Cats")
-    click_button 'Play'
-  end
-
   scenario 'view player 2s hit points' do
+    sign_in_and_play
     expect(page).to have_css('.hp', text: 'HP 100')
   end 
 
