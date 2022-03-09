09c1fe8fc38ec9c9cbe67a99a6f5f9989b36e678
Kathleen Ly <lykathleen@outlook.com>
2022-03-09 14:21:13 +0000

    Enabled sessions and refactored to use post and get requests

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Battle < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
@@ -9,12 +10,20 @@ class Battle < Sinatra::Base
   get '/' do
     erb :index
   end
-
+  
   post '/names' do
-    @player1 = params[:player_1]
-    @player2 = params[:player_2]
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect '/play'
+  end
+
+  get '/play' do 
+    @player1 = session[:player_1]
+    @player2 = session[:player_2]
     erb :play
   end
+
+
 
   run! if app_file == $0
 end
